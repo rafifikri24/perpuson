affb07448124d54a28b0e795b988f3a45a53865f
bagus <91867610+Bagussp717@users.noreply.github.com>
2024-03-25 22:09:38 -0700

    Update page.jsx

--- src/pages/admin/notification/page.jsx
+++ src/pages/admin/notification/page.jsx
@@ -12,16 +12,7 @@ export default function Notification() {
     const [modalShow, setModalShow] = useState(false);
     const [selectedNotifId, setSelectedNotifId] = useState(null);
 
-    const [noinduk, setNoinduk] = useState('');
-    const [kodebuku,setKodebuku] = useState('')
-    const [nama, setNama] = useState('');
-    const [prodi,setProdi] = useState('')
-    const [jumlahpinjam, setJumlahpinjam] = useState('');
-    const [tanggalpinjam, setTanggalpinjam] = useState('');
-    const [tanggalkembali, setTanggalkembali] = useState('');
-    const[buku,setBuku]=useState([])
-    const tanggal_Pinjam = new Date(tanggalpinjam).toLocaleDateString('id-ID')
-    const tanggal_Kembali = new Date(tanggalkembali).toLocaleDateString('id-ID')
+    const [data, setData] = useState([]);
 
     useEffect(() => {
         const isLoggedIn = localStorage.getItem('isLoggedIn')
@@ -34,7 +25,7 @@ export default function Notification() {
             }
           };
     
-          axios.get("https://perpus-smk-delta.vercel.app/notif", config)
+          axios.get("http://localhost:5000/notif", config)
             .then((response) => {
               setNotif(response.data);
             })
@@ -51,9 +42,19 @@ export default function Notification() {
         setSelectedNotifId(notifId);
         setModalShow(true);
 
+        const data = {
+          read_status : '1'
+        }
+
+        const token = localStorage.getItem('tokenjwt');
+        const config = {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+        }
 
         try{
-          await axios.put(`https://perpus-smk-delta.vercel.app/readStatus/${notifId}`,data,config);
+          await axios.put(`http://localhost:5000/readStatus/${notifId}`,data,config);
         }
         catch (error) {
         console.error('Upload failed:', error);
@@ -67,70 +68,36 @@ export default function Notification() {
       }
   
   })
-   useEffect(()=>{
-      const fetchData = async()=>{
-          try{
-              const response = await axios.get(`https://perpus-smk-delta.vercel.app/tampil/trx/${selectedNotifId}`)
-              setNoinduk(response.data[0].no_induk)
-              setKodebuku(response.data[0].kode_buku)
-              setJumlahpinjam(response.data[0].jumlah_pinjam)
-              setTanggalpinjam(response.data[0].tanggal_pinjam)
-              setTanggalkembali(response.data[0].tanggal_kembali)
-          } catch (error){
-              console.log(error)
-          }
-      };
-      fetchData();
-  },[selectedNotifId])
-
-  useEffect(()=>{
-      const token = localStorage.getItem('tokenjwt');
-      
-      if (token) {
-        const config = {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        };
-      axios.get(`https://perpus-smk-delta.vercel.app/tampil/siswa/${noinduk}`,config)
-      .then((response)=>{
-          setNama(response.data[0].nama)
-          setProdi(response.data[0].prodi)
-      })
-      .catch((error)=>{
-        console.log(error)
-      })
-    } else {
-      console.log('not login')
-      router.push('/');
-    }
-  },[noinduk])
 
   useEffect(() => {
-      const isLoggedIn = localStorage.getItem('isLoggedIn')
-      const token = localStorage.getItem('tokenjwt');
-      
-      if (token) {
-        const config = {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        };
-  
-        axios.get(`https://perpus-smk-delta.vercel.app/tampil/book/${kodebuku}`, config)
-          .then((response) => {
-            setBuku(response.data[0].judul_buku); // Assuming setBuku is a function to set your book data
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      } else {
-        console.log('not login')
-        router.push('/');
-      }
-    }, [])
+    const token = localStorage.getItem('tokenjwt');
+    
+    if (token) {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
+    axios.get("http://localhost:5000/tampil/stok/all-history",config)
+    .then((response)=>{
+      setData(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }else {
+    console.log('not login')
+    router.push('/');
+  }
+},[])
 
-    function ModalFunction(props) {
+function ModalFunction(props) {
+  if (!selectedNotifId) {
+    return null;
+  }
+  const selectedTransaction = data.find(item => item.kode_transaksi === selectedNotifId);
+  const tanggal_Pinjam = new Date(selectedTransaction.tanggal_pinjam).toLocaleDateString('id-ID')
+  const tanggal_Kembali = new Date(selectedTransaction.tanggal_kembali).toLocaleDateString('id-ID')
   return (
     <Modal
       {...props}
@@ -144,45 +111,46 @@ export default function Notification() {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Dari : {nama}</h4>
+          <h4>Dari : {selectedTransaction.nama}</h4>
           <table class="table">
-            <tbody>
-              <tr>
-                <th scope="row">Kode Tranasaksi Pinjam</th>
-                <td>{selectedNotifId}</td>
-              </tr>
-              <tr>
-                <th scope="row">No induk Peminjam</th>
-                <td>{noinduk}</td>
-              </tr>
-              <tr>
-                <th scope="row">Nama Peminjam</th>
-                <td>{nama}</td>
-              </tr>
-              <tr>
-                <th scope="row">Prodi</th>
-                <td>{prodi}</td>
-              </tr>
-              <tr>
-                <th scope="row">Kode Buku Yang di Pinjam</th>
-                <td>{kodebuku}</td>
-              </tr>
-              <tr>
-                <th scope="row">Judul Buku</th>
-                <td>{buku}</td>
-              </tr>
-              <tr>
-                <th scope="row">Jumlah Pinjam</th>
-                <td>{jumlahpinjam}</td>
-              </tr>
-              <tr>
-                <th scope="row">Tangal Pinjam</th>
-                <td>{tanggal_Pinjam}</td>
-              </tr>
-              <tr>
-                <th scope="row">Tangal Kembali</th>
-                <td>{tanggal_Kembali}</td>
-              </tr>
+            <tbody>            
+                  
+                    <tr>
+                    <th scope="row">Kode Tranasaksi Pinjam</th>
+                    <td>{selectedTransaction.kode_transaksi}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">No induk Peminjam</th>
+                    <td>{selectedTransaction.no_induk}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Nama Peminjam</th>
+                    <td>{selectedTransaction.nama}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Prodi</th>
+                    <td>{selectedTransaction.prodi}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Kode Buku Yang di Pinjam</th>
+                    <td>{selectedTransaction.kode_buku}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Judul Buku</th>
+                    <td>{selectedTransaction.judul_buku}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Jumlah Pinjam</th>
+                    <td>{selectedTransaction.jumlah_pinjam}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Tangal Pinjam</th>
+                    <td>{tanggal_Pinjam}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Tangal Kembali</th>
+                    <td>{tanggal_Kembali}</td>
+                  </tr>
             </tbody>
           </table>
       </Modal.Body>
@@ -193,52 +161,36 @@ export default function Notification() {
     </Modal>
   );
 }
+return(
+  <AdminLayout>
+      <div className="p-3 ">
+        <table className="table">
+          <tbody>
+            {notif.map((item) => {
+              const tanggal_notif = new Date(item.creation_time).toLocaleDateString('id-ID');
+              return(
+                <tr key={item.id}>
+                  <th style={{ fontWeight: item.read_status === 0 ? 'bold' : 'normal' }}>{item.message}</th>
+                  <th style={{ fontWeight: item.read_status === 0 ? 'bold' : 'normal' }}>{tanggal_notif}</th>
+                  <td>
+                    <Button variant="primary" onClick={() => handleOpenModal(item.kode_transaksi)}>
+                      Baca
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <ModalFunction
+            show={modalShow}
+            onHide={() => {
+              setModalShow(false);
+              window.location.reload();
+            }}
+        />
+      </div>
+  </AdminLayout>
+);
 
-    return(
-        <AdminLayout>
-            <div className="p-3 ">
-            <table className="table">
-                <tbody>
-                {notif.map((item) => {
-                        const status = item.read_status
-                        if (status === 0) {
-                          return(
-                            <tr key={item.id}>
-                            <th>{item.message}</th>
-                            <th>{item.creation_time}</th>
-                            <td>
-                                <Button variant="primary" onClick={() => handleOpenModal(item.kode_transaksi)}>
-                                    Baca
-                                </Button>
-                            </td>
-                        </tr>
-                        )
-                        }else{
-                          return(
-                            <tr key={item.id}>
-                            <td>{item.message}</td>
-                            <td>{item.creation_time}</td>
-                            <td>
-                                <Button variant="primary" onClick={() => handleOpenModal(item.kode_transaksi)}>
-                                    Baca
-                                </Button>
-                            </td>
-                        </tr>
-                        )
-                        }
-                                                   
-                        
-                    })}
-                </tbody>
-            </table>
-            <ModalFunction
-                show={modalShow}
-                onHide={() => {
-                  setModalShow(false);
-                  window.location.reload();
-              }}
-            />
-            </div>
-        </AdminLayout>
-    )
 }
