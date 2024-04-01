d86a0e4206a8946da42a5f87d52d5d83588de8e1
Rafi Fikrian N <117027265+rafifikri24@users.noreply.github.com>
2024-04-01 09:13:59 +0700

    Update edit.jsx

--- src/pages/admin/editadmin/edit.jsx
+++ src/pages/admin/editadmin/edit.jsx
@@ -2,14 +2,16 @@ import AdminLayout from "@/components/admin/AdminLayout"
 import { useRouter } from "next/router"
 import "bootstrap/dist/css/bootstrap.css"
 import { useState, useEffect } from "react"
-import axios from "axios"
+import axios from "axios";
+import Modal from "react-bootstrap/Modal";
 
 const Edit = () =>{
     const router = useRouter()
     const [user, setUser] = useState('');
     const [pass, setPass] = useState('');
     const [chatId, setChatId] = useState('');
- ////////////////
+    const [show, setShow] = useState(false);
+ 
 
  useEffect(() => {
     const token = localStorage.getItem('tokenjwt');
@@ -60,11 +62,12 @@ const handleSubmit =async(a)=>{
     } catch (error) {
         console.error(error);
     }
-   
 
     
- };
+};
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <>
         <AdminLayout>
@@ -75,7 +78,11 @@ const handleSubmit =async(a)=>{
                         <input type="text" class="form-control" id="Username" placeholder="Username" value={user} onChange={(a) => setUser(a.target.value)}/>
                     </div>
                     <div class="mb-3">
-                        <label for="Telegram" class="form-label">Chat ID Telegram</label>
+                        <label for="Telegram" class="form-label">Chat ID Telegram
+                        <span className="pe-auto" style={{display: "inline-block", padding: "0 5px", cursor: "help"}} onClick={handleShow}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 256 256"><path fill="#666666" d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24m0 192a88 88 0 1 1 88-88a88.1 88.1 0 0 1-88 88m16-40a8 8 0 0 1-8 8a16 16 0 0 1-16-16v-40a8 8 0 0 1 0-16a16 16 0 0 1 16 16v40a8 8 0 0 1 8 8m-32-92a12 12 0 1 1 12 12a12 12 0 0 1-12-12"/></svg>
+                        </span> 
+                        </label>
                         <input type="text" class="form-control" id="Telegram" placeholder="Chat ID Telegram" value={user} onChange={(a) => setUser(a.target.value)}/>
                     </div>
                     <div class="mb-3">
@@ -85,6 +92,25 @@ const handleSubmit =async(a)=>{
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        Cara mendapatkan Chat ID Telegram
+                    </Modal.Title>
+                </Modal.Header>
+                    <div className="modal-dialog modal-dialog-scrollable">
+                        <label class="form-label">1. Buka aplikasi Telegram.</label>
+                        <br />
+                        <label class="form-label">2. Klik tombol search/pencarian.</label>
+                        <img src="/tutor1.jpg" className="rounded" alt="tutorial" style={{ marginRight: "10px" }} width={350} height={350}/>
+                        <br />
+                        <label class="form-label">3. Ketik {"'Get My ID'"} pada kolom pencarian.</label>
+                        <img src="/tutor2.jpg" className="rounded" alt="tutorial" style={{ marginRight: "10px" }} width={350} height={350}/>
+                        <br />
+                        <label class="form-label">4. Ketik {"'/start'"} dan kirim. User ID/Chat ID berhasil di dapatkan.</label>
+                        <img src="/tutor3.jpg" className="rounded" alt="tutorial" style={{ marginRight: "10px" }} width={350} height={350}/>
+                    </div>
+            </Modal>
         </AdminLayout>
         </>
     )
