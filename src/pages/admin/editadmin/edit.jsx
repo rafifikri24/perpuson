239347b2edc98440b273d70ef4b2a8f99cc98908
Rafi Fikrian N <117027265+rafifikri24@users.noreply.github.com>
2024-04-01 09:47:52 +0700

    Update edit.jsx

--- src/pages/admin/editadmin/edit.jsx
+++ src/pages/admin/editadmin/edit.jsx
@@ -23,12 +23,13 @@ const Edit = () =>{
  useEffect(()=>{
     const fetchData = async()=>{
         const token = localStorage.getItem('tokenjwt');
+        const username = localStorage.getItem('username');
         const config = {
             headers: {
               Authorization: `Bearer ${token}`
             }}
         try{
-            const response = await axios.get(`https://perpus-smk-delta.vercel.app/tampil/admin`,config)
+            const response = await axios.get(`https://perpus-smk-delta.vercel.app/tampil/admin/${username}`,config)
             setUser(response.data[0].username)
             setPass(response.data[0].password)
             setChatId(response.data[0].chatId)
@@ -57,7 +58,7 @@ const handleSubmit =async(a)=>{
           }}
 
     try {
-        const response = await axios.put(`https://perpus-smk-delta.vercel.app/edit/admin/1`,data,config);
+        const response = await axios.put(`https://perpus-smk-delta.vercel.app/edit/admin/${username}`,data,config);
         window.location='/admin/home'
     } catch (error) {
         console.error(error);
@@ -83,7 +84,7 @@ const handleSubmit =async(a)=>{
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 256 256"><path fill="#666666" d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24m0 192a88 88 0 1 1 88-88a88.1 88.1 0 0 1-88 88m16-40a8 8 0 0 1-8 8a16 16 0 0 1-16-16v-40a8 8 0 0 1 0-16a16 16 0 0 1 16 16v40a8 8 0 0 1 8 8m-32-92a12 12 0 1 1 12 12a12 12 0 0 1-12-12"/></svg>
                         </span> 
                         </label>
-                        <input type="text" class="form-control" id="Telegram" placeholder="Chat ID Telegram" value={user} onChange={(a) => setUser(a.target.value)}/>
+                        <input type="text" class="form-control" id="Telegram" placeholder="Chat ID Telegram" value={chatId} onChange={(a) => setChatId(a.target.value)}/>
                     </div>
                     <div class="mb-3">
                         <label for="Prodi" class="form-label">Password</label>
