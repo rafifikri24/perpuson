2a327aaefefb484d21715fe9659ec253cb70327e
Rafi Fikrian N <117027265+rafifikri24@users.noreply.github.com>
2024-04-03 07:32:25 +0700

    Update [editsiswa].jsx

--- src/pages/admin/siswa/[editsiswa].jsx
+++ src/pages/admin/siswa/[editsiswa].jsx
@@ -9,7 +9,7 @@ const Edit = () =>{
     const { editsiswa } = router.query
     const [nama, setNama] = useState('');
     const [prodi,setProdi] = useState('')
-    const [noPeminjam, setNoPeminjam] = useState('');
+    const [noInduk, setNoInduk] = useState('');
     const [username, setUsername] = useState('');
     const [passwd, setPasswd] = useState('');
  
@@ -25,8 +25,12 @@ const Edit = () =>{
     const fetchData = async()=>{
         try{
             const response = await axios.get(`https://perpus-smk-delta.vercel.app/tampil/siswa/${editsiswa}`)
+            setNoInduk(response.data[0].no_induk)
             setNama(response.data[0].nama)
             setProdi(response.data[0].prodi)
+            setUsername(response.data[0].username)
+            setPasswd(response.data[0].password)
+            
 
         } catch (error){
             console.log(error)
@@ -38,8 +42,11 @@ const Edit = () =>{
 const handleSubmit =async(a)=>{
     a.preventDefault();
     const data = { 
+        no_induk : no_induk,
         nama : nama,
         prodi : prodi,
+        username : username,
+        password : password
 
     }
     console.log(data)
@@ -69,7 +76,7 @@ const handleSubmit =async(a)=>{
                 <form onSubmit={handleSubmit}>
                     <div class="mb-3">
                         <label for="Nama" class="form-label">No Peminjam</label>
-                        <input type="text" class="form-control" id="Peminjam" placeholder="Enter No Pinjam" value={nama} onChange={(a) => setNama(a.target.value)}/>
+                        <input type="text" class="form-control" id="Peminjam" placeholder="Enter No Pinjam" value={noInduk} onChange={(a) => setNama(a.target.value)}/>
                     </div>
                     <div class="mb-3">
                         <label for="Nama" class="form-label">Nama Peminjam</label>
@@ -81,11 +88,11 @@ const handleSubmit =async(a)=>{
                     </div>
                     <div class="mb-3">
                         <label for="Prodi" class="form-label">Username</label>
-                        <input type="text" class="form-control" id="Username" placeholder="Enter Username" value={prodi} onChange={(a) => setProdi(a.target.value)}/>
+                        <input type="text" class="form-control" id="Username" placeholder="Enter Username" value={username} onChange={(a) => setProdi(a.target.value)}/>
                     </div>
                     <div class="mb-3">
                         <label for="Prodi" class="form-label">Password</label>
-                        <input type="text" class="form-control" id="Password" placeholder="Enter Password" value={prodi} onChange={(a) => setProdi(a.target.value)}/>
+                        <input type="text" class="form-control" id="Password" placeholder="Enter Password" value={passwd} onChange={(a) => setProdi(a.target.value)}/>
                     </div>
                     <div class="mb-3 form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
