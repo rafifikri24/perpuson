2286431f8f42e9f04c09d4001db90d229635bc0f
bagus <91867610+Bagussp717@users.noreply.github.com>
2024-04-05 22:03:04 +0700

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Space+Mono&display=swap" rel="stylesheet"></link>
       <Head />
       <body>
         <Main />
