126f7e37e51f6971e1b6dae39fe4bd87f7fbe301
bagus <91867610+Bagussp717@users.noreply.github.com>
2024-04-05 09:21:43 +0700

    Update AdminLayout.jsx

--- src/components/admin/AdminLayout.jsx
+++ src/components/admin/AdminLayout.jsx
@@ -5,6 +5,28 @@ import useAuth from "./protector"
 import {useEffect} from "react"
 import axios from "axios"
 const AdminLayout = ({children}) => {
+    useEffect(() => {
+    const token = localStorage.getItem('tokenjwt');
+    
+    if (token) {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
+
+      axios.get("https://perpus-smk-delta.vercel.app/get-me", config)
+        .then((response) => {
+            useAuth(response.data.role)
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      console.log('not login')
+      router.push('/');
+    }
+  }, [])
     return (
         <>   <Head>
                 <title>Perpustakan SMK PGRI 1 Banyuwangi</title>
