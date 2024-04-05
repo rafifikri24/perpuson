925a9978a9162a9e39645e90a1333d38b808d3ae
Rafi Fikrian N <117027265+rafifikri24@users.noreply.github.com>
2024-04-06 05:04:26 +0700

    Update [peminjaman].jsx

--- src/pages/admin/pinjam/pinjambynoinduk/[peminjaman].jsx
+++ src/pages/admin/pinjam/pinjambynoinduk/[peminjaman].jsx
@@ -6,11 +6,19 @@ import axios from "axios"
 
 const Peminjaman = () =>{
     const router = useRouter()
+    const getReturnDate = (tanggalpinjam, jumlahHari) => {
+        const returnDate = new Date(tanggalpinjam);
+        returnDate.setDate(returnDate.getDate() + jumlahHari);
+        return returnDate
+      };
     const { peminjaman } = router.query
+    const currentDate = new Date().toISOString().slice(0, 10);
+    const [jumlahHari, setJumlahHari ] = useState('')
     const [noinduk, setNoinduk] = useState('');
     const [jumlahpinjam, setJumlahpinjam] = useState('');
-    const [tanggalpinjam, setTanggalpinjam] = useState('');
-    const [tanggalkembali, setTanggalkembali] = useState('');
+    const [tanggalpinjam, setTanggalpinjam] = useState(currentDate);
+    const [tanggalkembali, setTanggalkembali] = useState(getReturnDate(currentDate, jumlahHari));
+    const kembali = tanggalkembali.toISOString().split('T')[0]
     const [nextId, setNextId] = useState('');
     const [namaPeminjam,setNamaPeminjam] = useState('')
     const [judul, setJudul] = useState('');
@@ -73,7 +81,7 @@ const handleSubmit =async(a)=>{
         status_pinjam : 1,
         jumlah_pinjam : jumlahpinjam,
         tanggal_pinjam : tanggalpinjam,
-        tanggal_kembali : tanggalkembali,
+        tanggal_kembali : kembali,
 
     }
     const token = localStorage.getItem('tokenjwt');
@@ -96,7 +104,7 @@ const handleSubmit =async(a)=>{
       setTahunterbit('')
       setJumlahpinjam('')
       setTanggalpinjam('')
-      setTanggalkembali('')
+      // setTanggalkembali('')
       setNoinduk('')
       setNamaPeminjam('')
       window.location='/admin/pinjam/daftarpeminjaman'
@@ -128,58 +136,71 @@ const handleSubmit =async(a)=>{
     return (
         <>
             <AdminLayout>
-                <form onSubmit={handleSubmit}>
-                    <h1>Form Peminjaman Buku</h1>
-                    <div class="mb-3">
-                        <label for="No" class="form-label">Kode Transaksi</label>
-                        <input type="text" class="form-control" id="No" placeholder="Kode Transaksi" value={nextId} onChange={(a) => setNextId(a.target.value)}/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="Nama" class="form-label">Kode Peminjam</label>
-                        <input disabled type="text" class="form-control" id="Nama" placeholder="No Induk"value={noinduk} onChange={(a) => setNoinduk(a.target.value)}/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="Nama" class="form-label">Nama Peminjam</label>
-                        <input disabled type="text" class="form-control" id="Nama" placeholder="Nama Peminjam"value={namaPeminjam} onChange={(a) => setNamaPeminjam(a.target.value)}/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="Prodi" class="form-label">Kode Buku</label>
-                        <input required type="text" class="form-control" id="Prodi" placeholder="Kode Buku"value={kode} onChange={(a) => {setKode(a.target.value);fetchIdentitasBuku(a.target.value)}}/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="Prodi" class="form-label">Judul Buku</label>
-                        <input disabled type="text" class="form-control" id="Prodi" placeholder="Judul Buku"value={judul} onChange={(a) => setJudul(a.target.value)}/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="Prodi" class="form-label">Pengarang Buku</label>
-                        <input disabled type="text" class="form-control" id="Prodi" placeholder="Pengarang Buku"value={pengarang} onChange={(a) => setPengarang(a.target.value)}/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="Prodi" class="form-label">Penerbit Buku</label>
-                        <input disabled type="text" class="form-control" id="Prodi" placeholder="Penerbit Buku"value={penerbit} onChange={(a) => setPenerbit(a.target.value)}/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="Prodi" class="form-label">Tahun Terbit</label>
-                        <input disabled type="text" class="form-control" id="Prodi" placeholder="Tahun Terbit"value={tahunterbit} onChange={(a) => setTahunterbit(a.target.value)}/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="Prodi" class="form-label">Jumlah Pinjam</label>
-                        <input required type="text" class="form-control" id="Prodi" placeholder="Jumlah Pinjam"value={jumlahpinjam} onChange={(a) => setJumlahpinjam(a.target.value)}/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="Prodi" class="form-label">Tanggal Pinjam</label>
-                        <input required type='date' class="form-control" id="Prodi" placeholder="Tanggal Pinjam"value={tanggalpinjam} onChange={(a) => setTanggalpinjam(a.target.value)}/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="Prodi" class="form-label">Tanggal Kembali</label>
-                        <input required type='date' class="form-control" id="Prodi" placeholder="Tanggal Kembali"value={tanggalkembali} onChange={(a) => setTanggalkembali(a.target.value)}/>
-                    </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1" required/>
-                        <label class="form-check-label" for="exampleCheck1">Anda Yakin Sudah Benar ?</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary" >Submit</button>
-                </form>
+                <div className="p-4">
+                    <form onSubmit={handleSubmit}>
+                        <h1>Form Peminjaman Buku</h1>
+                        <div class="mb-3">
+                            <label for="No" class="form-label">Kode Transaksi</label>
+                            <input type="text" class="form-control" id="No" placeholder="Kode Transaksi" value={nextId} onChange={(a) => setNextId(a.target.value)}/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="Nama" class="form-label">Kode Peminjam</label>
+                            <input disabled type="text" class="form-control" id="Nama" placeholder="No Induk"value={noinduk} onChange={(a) => setNoinduk(a.target.value)}/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="Nama" class="form-label">Nama Peminjam</label>
+                            <input disabled type="text" class="form-control" id="Nama" placeholder="Nama Peminjam"value={namaPeminjam} onChange={(a) => setNamaPeminjam(a.target.value)}/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="Prodi" class="form-label">Kode Buku</label>
+                            <input required type="text" class="form-control" id="Prodi" placeholder="Kode Buku"value={kode} onChange={(a) => {setKode(a.target.value);fetchIdentitasBuku(a.target.value)}}/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="Prodi" class="form-label">Judul Buku</label>
+                            <input disabled type="text" class="form-control" id="Prodi" placeholder="Judul Buku"value={judul} onChange={(a) => setJudul(a.target.value)}/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="Prodi" class="form-label">Pengarang Buku</label>
+                            <input disabled type="text" class="form-control" id="Prodi" placeholder="Pengarang Buku"value={pengarang} onChange={(a) => setPengarang(a.target.value)}/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="Prodi" class="form-label">Penerbit Buku</label>
+                            <input disabled type="text" class="form-control" id="Prodi" placeholder="Penerbit Buku"value={penerbit} onChange={(a) => setPenerbit(a.target.value)}/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="Prodi" class="form-label">Tahun Terbit</label>
+                            <input disabled type="text" class="form-control" id="Prodi" placeholder="Tahun Terbit"value={tahunterbit} onChange={(a) => setTahunterbit(a.target.value)}/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="Prodi" class="form-label">Jumlah Pinjam</label>
+                            <input required type="text" class="form-control" id="Prodi" placeholder="Jumlah Pinjam"value={jumlahpinjam} onChange={(a) => setJumlahpinjam(a.target.value)}/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="Prodi" class="form-label">Tanggal Pinjam</label>
+                            <input required type='date' class="form-control" id="Prodi" placeholder="Tanggal Pinjam" value={tanggalpinjam} disabled min={currentDate} onChange={(a) => setTanggalpinjam(a.target.value)}/>
+                        </div>
+                        <div class="mb-3">
+                        <label for="Prodi" class="form-label">Lama Peminjaman</label>
+                            <select class="form-select" aria-label="Default select example" value={jumlahHari} onChange={handledayschange}>
+                                <option selected value="" >Pilih</option>
+                                <option value={3}>3 hari</option>
+                                <option value={5}>5 hari</option>
+                                <option value={10}>10 hari</option>
+                                <option value={15}>15 hari</option>
+                                <option value={20}>20 hari</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="Prodi" class="form-label">Tanggal Kembali</label>
+                            <input required type='date' class="form-control" id="Prodi" placeholder="Tanggal Kembali" disabled value={kembali} onChange={(a) => setTanggalkembali(a.target.value)}/>
+                        </div>
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1" required/>
+                            <label class="form-check-label" for="exampleCheck1">Anda Yakin Sudah Benar ?</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary" >Submit</button>
+                    </form>
+                </div>
             </AdminLayout>
         </>
     )
