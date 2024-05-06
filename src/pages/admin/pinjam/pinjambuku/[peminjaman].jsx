0e3216a3aed3e818ffb036e2c3ee95c232406162
Rafi Fikrian N <117027265+rafifikri24@users.noreply.github.com>
2024-05-06 11:58:01 +0700

    Update [peminjaman].jsx

--- src/pages/admin/pinjam/pinjambuku/[peminjaman].jsx
+++ src/pages/admin/pinjam/pinjambuku/[peminjaman].jsx
@@ -181,7 +181,7 @@ const handleSubmit =async(a)=>{
                             <input required type="text" class="form-control" id="Prodi" placeholder="Jumlah Pinjam" value={jumlahpinjam} onChange={(a) => setJumlahpinjam(a.target.value)}/>
                         </div>
                         <div class="mb-3">
-                            <label for="Prodi" class="form-label">Tanggal Pinjam</label>
+                            <label for="Prodi" class="form-label">Tanggal Pinjam <h6>(Bulan / Tanggal / Tahun)</h6></label>
                             <input required type='date' class="form-control" id="Prodi" placeholder="Tanggal Pinjam" value={tanggalpinjam} disabled min={currentDate} onChange={(a) => setTanggalpinjam(a.target.value)}/>
                         </div>
                         <div class="mb-3">
@@ -202,7 +202,7 @@ const handleSubmit =async(a)=>{
                             </select>
                         </div>
                         <div class="mb-3">
-                            <label for="Prodi" class="form-label">Tanggal Kembali</label>
+                            <label for="Prodi" class="form-label">Tanggal Kembali <h6>(Bulan / Tanggal / Tahun)</h6></label>
                             <input required type='date' class="form-control" id="Prodi" placeholder="Tanggal Kembali" disabled value={kembali} onChange={(a) => setTanggalkembali(a.target.value)}/>
                         </div>
                         <div class="mb-3 form-check">
