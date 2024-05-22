84655339f5929da14845fd6b8c3d258167c1e771
Rafi Fikrian N <117027265+rafifikri24@users.noreply.github.com>
2024-05-22 12:40:22 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
 
       axios.get("https://perpus-smk-delta.vercel.app/get-me", config)
         .then((response) => {
-            window.location.href = response.data.redirectUrl
+            window.location.href = response.data
         })
         .catch((error) => {
           console.log(error);
@@ -46,8 +46,11 @@ export default function Home() {
             <div className="cover-content">
               <h1>Perpustakan SMK PGRI 1 Banyuwangi</h1>
               <p>Selamat datang di Perpustakaan Online SMK PGRI 1 Banyuwangi! Temukan koleksi buku kami secara cepat dan mudah. Pesan dan nikmati buku yang anda pinjam. Mulailah perjalanan literasi Anda sekarang!</p>
-              <Button variant="primary"><a href="login" className="text-decoration-none text-light">Log In</a></Button>
               
+              <div className="d-flex align-items-center flex-column gap-3">
+              <Button variant='primary' className="custom-button"><a href="login" className="text-decoration-none text-light">Log In</a></Button>
+              <Button variant='success'><a href="aplikasi" className="text-decoration-none text-light d-flex align-items-center"><label className="px-2" style={{cursor: 'pointer'}}>Unduh APK</label><svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><path fill="white" d="m12 16l-5-5l1.4-1.45l2.6 2.6V4h2v8.15l2.6-2.6L17 11zm-6 4q-.825 0-1.412-.587T4 18v-3h2v3h12v-3h2v3q0 .825-.587 1.413T18 20z"/></svg></a></Button>
+              </div>
             </div>
           </Container>
         </section>
@@ -103,7 +106,6 @@ export default function Home() {
             text-align: center;
             padding: 20px 0;
           }
-
         `}</style>
       </div>
     </>
